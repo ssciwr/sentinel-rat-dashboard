b0ba3177a9bd1aab93b9855d10c200c4feda3a10
tuyenle <lethikimtuyen87@gmail.com>
2026-08-20 11:08:13 +0200

    change to python 3.12 and try to add R test into CI pipeline

--- tests/testthat/test-db.R
+++ tests/testthat/test-db.R
@@ -1,5 +1,5 @@
 test_that("fetch_analyses returns empty list initially", {
   result <- fetch_analyses()
-  expect_type(result, "list")
-  expect_length(result, 0)
+  expect_s3_class(result, "data.frame")
+  expect_nrow(result, 0)
 })
