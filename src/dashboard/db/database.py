67badeaf083bf5f878dbb8ad2130fa999a0a391d
tuyenle <lethikimtuyen87@gmail.com>
2026-08-20 11:56:19 +0200

    use env values for database url

--- src/dashboard/db/database.py
+++ src/dashboard/db/database.py
@@ -4,9 +4,16 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
+POSTGRES_DB = os.getenv("POSTGRES_DB", "sentinel_db")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "sentinel_user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "sentinel_pass")
+
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+psycopg://sentinel_user:sentinel_pass@db:5432/sentinel_db",
+    f"postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}",
 )
 
 engine = create_engine(DATABASE_URL, echo=False)
